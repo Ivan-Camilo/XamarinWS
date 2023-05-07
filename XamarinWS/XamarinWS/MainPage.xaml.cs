47cb2ca4a608c5b3169eab8a303adb81dd3f685f
Ivan-Camilo <ivan.rivera.s@gmail.com>
2023-05-07 14:38:38 -0500

    Primer Commit

--- XamarinWS/XamarinWS/MainPage.xaml.cs
+++ XamarinWS/XamarinWS/MainPage.xaml.cs
@@ -1,18 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace XamarinWS
 {
     public partial class MainPage : ContentPage
     {
+
+        private const string Url = "http://192.168.100.12/semana5/post.php"; //tiene que ser la IP de maquina
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<XamarinWS.Ws.Datos> _post;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<XamarinWS.Ws.Datos> posts = JsonConvert.DeserializeObject<List<XamarinWS.Ws.Datos>>(content);
+            _post = new ObservableCollection<XamarinWS.Ws.Datos>(posts);
+            MyListView.ItemsSource = _post;
         }
     }
 }
