1e03eecf78719865766b32385d7bb841a0068a7d
Ivan-Camilo <ivan.rivera.s@gmail.com>
2023-05-07 14:38:18 -0500

    Primer Commit

---
+++ XamarinWS/XamarinWS/Ws/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace XamarinWS.Ws
+{
+    public class Datos
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string ubicacion { get; set; }
+        public float extension { get; set; }
+        public string tipo { get; set; }
+        public string variedad { get; set; }
+        public string encargado { get; set; }
+        public int ncamas { get; set; }
+    }
+}
+
